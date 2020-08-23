55d533017686e18f8fcb8d5da6020342198d6db1
Zubs <zubairidrisaweda@gmail.com>
2020-08-23 23:27:21 +0100

    Made progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,67 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
       body: Center(
-        child: Text(
-          'Hello World',
-          style: TextStyle(
-            fontSize: 40,
-            color: Colors.red,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 4,
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.library_books,
+                  size: 80,
+                  color: Colors.green,
+                ),
+                Text(
+                  'iTeach',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 40,
+                    letterSpacing: 4,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  child: RaisedButton.icon(
+                    color: Colors.green,
+                    onPressed:() {},
+                    icon: Icon(
+                      Icons.weekend,
+                      color: Colors.red,
+                      size: 40,
+                    ),
+                    label: Text(
+                      'Login',
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 20,
+                        letterSpacing: 4,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
         ),
       ),
-    ),
-  ));
+    );
+  }
 }
